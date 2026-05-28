771406a9e6f01bc618f6343642525fbcf72248ee
alexpf-maker <alexandroperez.fierro@gmail.com>
2026-05-27 19:06:40 -0700

    Agrega mensaje al botón principal

--- SistemaEscolar/SistemaEscolar/frmPrincipal.cs
+++ SistemaEscolar/SistemaEscolar/frmPrincipal.cs
@@ -6,5 +6,10 @@ namespace SistemaEscolar
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Es la pantalla principal");
+        }
     }
 }
